38a69091ee705288d4ebc9d33a40d892ce42bf7a
gERS-PRO <ersultantalapkhan@gmail.com>
2025-12-11 19:41:04 +0500

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,18 @@
 # courses/urls.py
 from django.urls import path
-from .views import CourseListView
+from .views import CourseListView, CourseDetailView, enroll_course, my_courses
 
 app_name = 'courses'
 
 urlpatterns = [
-    # Главная страница сайта (/)
     path('', CourseListView.as_view(), name='course_list'),
+    
+    # Страница одного курса (например: /course/1/)
+    path('course/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
+    
+    # Действие записи на курс
+    path('course/<int:pk>/enroll/', enroll_course, name='enroll_course'),
+    
+    # Список моих курсов
+    path('my-courses/', my_courses, name='my_courses'),
 ]
